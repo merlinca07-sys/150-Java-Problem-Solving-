27fda36d2e1d25a28296c067e003b61488c2b85d
MERLIN C A <merlinca07@gmail.com>
2026-06-30 21:16:58 +0530

    code to count even number

---
+++ EvenNumbers.java
@@ -0,0 +1,13 @@
+class EvenNumbers{
+    public static void main(String[] args){
+        int [] arr = {10,21,30,41,50};
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] %2==0)
+                count++;
+                   
+        }
+        System.out.println("The count  of even number present : " + count);
+    }
+    
+}
